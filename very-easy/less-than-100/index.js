9bbdad51e116ba35450dbd28179beb1abf36e78e
Inez Amandha <inezamandhasuci@gmail.com>
2024-06-06 21:18:56 +0700

    ✨ Add "Less Than 100?"

---
+++ very-easy/less-than-100/index.js
@@ -0,0 +1,5 @@
+function lessThan100(a, b) {
+	return a + b < 100 ? true : false
+}
+
+export default lessThan100
---
+++ very-easy/less-than-100/index.test.js
@@ -0,0 +1,28 @@
+
+
+import lessThan100 from './index';
+
+describe('Javascript - Very Easy - 20', () => {
+    test('Less Than 100?', () => {
+        expect(lessThan100(5, 57)).toEqual(true)
+        expect(lessThan100(77, 30)).toEqual(false)
+        expect(lessThan100(0, 59)).toEqual(true)
+        expect(lessThan100(78, 35)).toEqual(false)
+        expect(lessThan100(63, 11)).toEqual(true)
+        expect(lessThan100(37, 99)).toEqual(false)
+        expect(lessThan100(52, 11)).toEqual(true)
+        expect(lessThan100(82, 95)).toEqual(false)
+        expect(lessThan100(17, 44)).toEqual(true)
+        expect(lessThan100(74, 53)).toEqual(false)
+        expect(lessThan100(3, 77)).toEqual(true)
+        expect(lessThan100(25, 80)).toEqual(false)
+        expect(lessThan100(59, 28)).toEqual(true)
+        expect(lessThan100(69, 87)).toEqual(false)
+        expect(lessThan100(10, 45)).toEqual(true)
+        expect(lessThan100(43, 58)).toEqual(false)
+        expect(lessThan100(50, 44)).toEqual(true)
+        expect(lessThan100(74, 89)).toEqual(false)
+        expect(lessThan100(3, 27)).toEqual(true)
+        expect(lessThan100(21, 79)).toEqual(false)
+    });
+});
