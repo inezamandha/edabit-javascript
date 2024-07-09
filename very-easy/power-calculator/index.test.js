a8aa48bb4066c4421ccbc2459f92f14515addb0b
Inez Amandha <inezamandhasuci@gmail.com>
2024-07-09 11:16:09 +0700

    💬 Change test naming from Circuit Power to Power Calculator

--- very-easy/power-calculator/index.test.js
+++ very-easy/power-calculator/index.test.js
@@ -1,7 +1,7 @@
 import circuitPower from './index';
 
 describe('Javascript - Very Easy - 9', () => {
-  test('Circuit Power', () => {
+  test('Power Calculator', () => {
     expect(circuitPower(110, 3)).toEqual(330);
     expect(circuitPower(230, 10)).toEqual(2300);
     expect(circuitPower(480, 20)).toEqual(9600);
