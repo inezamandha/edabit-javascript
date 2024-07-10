b8b1ce864fb1eb4a4bd3c1dbe5de0b1e776acf5f
Inez Amandha <inezamandhasuci@gmail.com>
2024-07-10 11:35:52 +0700

    🔧 Add ESLint configuration

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,36 @@
+import prettier from 'eslint-plugin-prettier';
+import jest from 'eslint-plugin-jest';
 import globals from 'globals';
-import pluginJs from '@eslint/js';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import js from '@eslint/js';
+import { FlatCompat } from '@eslint/eslintrc';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+});
 
 export default [
-  { languageOptions: { globals: globals.browser } },
-  pluginJs.configs.recommended,
+  ...compat.extends('eslint:recommended', 'plugin:prettier/recommended'),
+  {
+    plugins: {
+      prettier,
+      jest,
+    },
+
+    languageOptions: {
+      globals: {
+        ...globals.jest,
+        ...jest.environments.globals.globals,
+      },
+    },
+
+    rules: {
+      'prettier/prettier': 'error',
+    },
+  },
 ];
