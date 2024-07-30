6cb8ac2fa1e8728bd7daab944cf2be1b5da716fb
Inez Amandha <inezamandhasuci@gmail.com>
2024-07-30 15:36:31 +0700

    ✨ Add "Repeating Letters"

---
+++ easy/repeating-letters/index.js
@@ -0,0 +1,9 @@
+function doubleChar(str) {
+  let result = '';
+  for (let char of str) {
+    result += char.repeat(2);
+  }
+  return result;
+}
+
+export default doubleChar;
---
+++ easy/repeating-letters/index.test.js
@@ -0,0 +1,20 @@
+import doubleChar from './index';
+
+describe('Javascript - Easy - 29', () => {
+  test('Repeating Letters', () => {
+    expect(doubleChar('String')).toEqual('SSttrriinngg');
+    expect(doubleChar('Hello World!')).toEqual('HHeelllloo  WWoorrlldd!!');
+    expect(doubleChar('1234!_ ')).toEqual('11223344!!__  ');
+    expect(doubleChar('##^&%%*&%%$#@@!')).toEqual(
+      '####^^&&%%%%**&&%%%%$$##@@@@!!'
+    );
+    expect(doubleChar('scandal')).toEqual('ssccaannddaall');
+    expect(doubleChar('economics')).toEqual('eeccoonnoommiiccss');
+    expect(doubleChar(' ')).toEqual('  ');
+    expect(doubleChar('_______')).toEqual('______________');
+    expect(doubleChar('equip gallon read')).toEqual(
+      'eeqquuiipp  ggaalllloonn  rreeaadd'
+    );
+    expect(doubleChar('baby increase')).toEqual('bbaabbyy  iinnccrreeaassee');
+  });
+});
