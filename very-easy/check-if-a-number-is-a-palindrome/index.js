f72c99817e5a69cdd2fe167efc56c4e279e91ca0
Inez Amandha <inezamandhasuci@gmail.com>
2024-09-10 20:52:03 +0700

    ♻️ Refactor code for "Check if a Number is a Palindrome"

--- very-easy/check-if-a-number-is-a-palindrome/index.js
+++ very-easy/check-if-a-number-is-a-palindrome/index.js
@@ -1,6 +1,6 @@
 function isPalindrome(n) {
   const palindromeCheck = n.toString().split('').reverse().join('');
-  return n.toString() === palindromeCheck ? true : false;
+  return n.toString() === palindromeCheck;
 }
 
 export default isPalindrome;
