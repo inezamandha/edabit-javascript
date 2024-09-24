102edacbeef4883298a4225116bb60d4b259f343
Inez Amandha <inezamandhasuci@gmail.com>
2024-09-24 16:15:33 +0700

    ✨ Add "Matchstick Houses"

---
+++ easy/matchstick-houses/index.js
@@ -0,0 +1,5 @@
+function matchHouses(step) {
+  return step === 0 ? 0 : 5 * step + 1;
+}
+
+export default matchHouses;
---
+++ easy/matchstick-houses/index.test.js
@@ -0,0 +1,14 @@
+import matchHouses from './index';
+
+describe('Javascript - Easy - 52', () => {
+  test('Matchstick Houses', () => {
+    expect(matchHouses(1)).toEqual(6);
+    expect(matchHouses(0)).toEqual(0);
+    expect(matchHouses(17)).toEqual(86);
+    expect(matchHouses(36)).toEqual(181);
+    expect(matchHouses(15)).toEqual(76);
+    expect(matchHouses(99)).toEqual(496);
+    expect(matchHouses(3)).toEqual(16);
+    expect(matchHouses(87)).toEqual(436);
+  });
+});
