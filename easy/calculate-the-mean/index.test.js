40aa84212d52e23d357c702d4d042fa568845b94
Inez Amandha <inezamandhasuci@gmail.com>
2024-09-25 16:55:34 +0700

    ✅ Update "Calculate the mean" test

--- easy/calculate-the-mean/index.test.js
+++ easy/calculate-the-mean/index.test.js
@@ -2,12 +2,12 @@ import mean from './index';
 
 describe('Javascript - Easy - 62', () => {
   test('Calculate the Mean', () => {
-    expect(mean([1, 0, 4, 5, 2, 4, 1, 2, 3, 3, 3])).toBeCloseTo(2.55, 2);
-    expect(mean([324, 543, 654, 9876])).toBeCloseTo(2849.25, 2);
-    expect(mean([0, 0, 0, 0])).toBeCloseTo(0.0, 2);
-    expect(mean([30, 40, 20, 100, 30])).toBeCloseTo(44.0, 2);
-    expect(mean([1, 1, 1, 0])).toBeCloseTo(0.75, 2);
-    expect(mean([1, 1, 0, 1, 2, 1, 1, 1, 0, 0])).toBeCloseTo(0.8, 2);
-    expect(mean([10000])).toBeCloseTo(10000.0, 2);
+    expect(mean([1, 0, 4, 5, 2, 4, 1, 2, 3, 3, 3])).toEqual(2.55);
+    expect(mean([324, 543, 654, 9876])).toEqual(2849.25);
+    expect(mean([0, 0, 0, 0])).toEqual(0.0);
+    expect(mean([30, 40, 20, 100, 30])).toEqual(44.0);
+    expect(mean([1, 1, 1, 0])).toEqual(0.75);
+    expect(mean([1, 1, 0, 1, 2, 1, 1, 1, 0, 0])).toEqual(0.8);
+    expect(mean([10000])).toEqual(10000.0);
   });
 });
